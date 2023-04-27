0c067f2a64ca7046bd22c3e67250386ade6c5d87
Kst <andrewsergeev98@gmil.com>
2023-04-27 18:16:44 +0300

    railway_1

--- src/main/java/com/sergeev/application/entity/Address.java
+++ src/main/java/com/sergeev/application/entity/Address.java
@@ -29,7 +29,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Table(name = "address", schema = "Aston_application")
+@Table(name = "address", schema = "railway")
 @NamedQueries({
         @NamedQuery(name = "Address.getAddressById", query = "select a from Address a left join fetch a.employeeList where a.id = :id"),
         @NamedQuery(name = "Address.getAddressByIdLazy", query = "select a from Address a where a.id = :id")})
